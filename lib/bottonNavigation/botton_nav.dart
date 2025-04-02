30f33acdd7d77d4394628f4bc1c251f084feb170
niraj <flutterproject28@gmail.com>
2025-04-02 18:07:50 +0530

    i did category work in home page.

---
+++ lib/bottonNavigation/botton_nav.dart
@@ -0,0 +1,47 @@
+
+import 'package:flutter/material.dart';
+import '../home_page_view/all_categories.dart';
+import '../home_page_view/home_screen.dart';
+class BottomNavScreen extends StatefulWidget {
+  const BottomNavScreen({super.key});
+
+  @override
+  State<BottomNavScreen> createState() => _BottomNavScreenState();
+}
+
+class _BottomNavScreenState extends State<BottomNavScreen> {
+  var index = 0;
+  List pagecontrooller = [
+    HomeScreen(),
+    AllCategoriesPage(),
+    HomeScreen(),
+    HomeScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pagecontrooller[index],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedFontSize: 15,
+          selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+          currentIndex: index,
+          type: BottomNavigationBarType.fixed,
+          onTap: (value) {
+            setState(() {
+              index = value;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+                label: 'Home', icon: Icon(Icons.home,size: 30,)),
+            BottomNavigationBarItem(
+                label: 'Category', icon: Icon(Icons.dashboard_customize_outlined,size: 30,)),
+            BottomNavigationBarItem(
+                label: 'Booking', icon: Icon(Icons.backpack,size: 30,)),
+            BottomNavigationBarItem(
+                label: 'Profile', icon: Icon(Icons.person,size: 30,)),
+          ]),
+    );
+  }
+}
