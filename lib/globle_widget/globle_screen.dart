f9ed6689f48ab79254b0920a9ccc5283cae91d8c
niraj <flutterproject28@gmail.com>
2025-04-02 18:07:50 +0530

    sdfghj

---
+++ lib/globle_widget/globle_screen.dart
@@ -0,0 +1,229 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class GlobleWidget {
+  static AllCategoryImage(BuildContext context, String text1, String text2,
+      image, VoidCallback onPressed) {
+    var widthScreen = MediaQuery.of(context).size.width * 2;
+    var heightScreen = MediaQuery.of(context).size.height * 1;
+    return InkWell(
+      onTap: onPressed,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 5,right: 5),
+        child: Card(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+              side: BorderSide(
+                color: Colors.grey.shade400,
+              )),
+          child: Row(
+            children: [
+              SizedBox(width: 20,),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image(
+                  image: AssetImage(image),
+                  height: heightScreen * 0.1,
+                  width: widthScreen * 0.1,
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+              SizedBox(width: 10,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    text1,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  Text(
+                    text2,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.grey.shade500),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  static AllCategory(
+      BuildContext context, String text, image, VoidCallback onPressed) {
+    return InkWell(
+      onTap: onPressed,
+      child: Card(
+        elevation: 0,
+        color: Colors.white,
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image(
+                image: AssetImage(image),
+                height: 120,
+                width: 120,
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            Text(
+              text,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  static BestServicesImage1(
+    BuildContext context,
+    String image,
+  ) {
+    var widthScreen = MediaQuery.of(context).size.width * 0.7;
+    var heightScreen = MediaQuery.of(context).size.height * 0.2;
+    return InkWell(
+      child: Card(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: Container(
+          height: heightScreen,
+          width: widthScreen,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            image: DecorationImage(
+              image: AssetImage(image),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  static BestServicesCircleAvatar(
+    BuildContext context,
+    String image,
+  ) {
+    return CircleAvatar(
+      radius: 25,
+      child: Image(
+        image: AssetImage(image),
+      ),
+    );
+  }
+
+  static ServicesPersonName(
+    BuildContext context,
+    String text,
+  ) {
+    return Text(text,
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold));
+  }
+
+  static ServicesProvide(
+    BuildContext context,
+    String text,
+  ) {
+    return Text(text,
+        style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal));
+  }
+
+  static WorkNameText(
+    BuildContext context,
+    String text,
+  ) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10),
+      child: Text(
+        text,
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  static SeeAllCategories(
+    VoidCallback onPressed,
+    BuildContext context,
+    String text,
+  ) {
+    return InkWell(
+      onTap: onPressed,
+      child: Text(
+        text,
+        style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue.shade500),
+      ),
+    );
+  }
+
+  static TextSpanTextOriginal(
+    BuildContext context,
+    String text1,
+    String text2,
+  ) {
+    return Padding(
+      padding: EdgeInsets.only(left: 10),
+      child: Text.rich(TextSpan(children: [
+        TextSpan(
+          // text: "₹150 \u00A0",
+          text: "₹${text1} \u00A0",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        TextSpan(
+            // text: "₹50",
+            text: "₹${text2}",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey,
+              decorationColor: Colors.red,
+              decorationThickness: 2,
+              decoration: TextDecoration.lineThrough,
+            )),
+      ])),
+    );
+  }
+
+  static ServicesProvideAddButton(
+    VoidCallback onPressed,
+    BuildContext context,
+    String text,
+  ) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5), // Border radius 5
+            ),
+          ),
+          onPressed: onPressed,
+          child: Text(
+            text,
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          )),
+    );
+  }
+
+  static ClickIcon(
+      VoidCallback onPressed, BuildContext context, IconData icon) {
+    return InkWell(
+      onTap: onPressed,
+      child: Icon(icon),
+    );
+  }
+}
