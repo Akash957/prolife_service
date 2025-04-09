42cd21d103406e602aec3f32e19e484f774b6d42
Akash957 <157791836+Akash957@users.noreply.github.com>
2025-04-09 17:33:43 +0530

    add more on category

--- lib/home_page_view/home_screen.dart
+++ lib/home_page_view/home_screen.dart
@@ -1,7 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:prolife_service/screens/profile_screen.dart';
+import 'package:get/get.dart';
+import '../getx_service/getx_screen.dart';
 import '../global_widget/globle_screen.dart';
+import 'all_categories.dart';
+import 'click_on_categories.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,263 +14,128 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final getController = Get.put(GetService());
+
+  @override
+  void initState() {
+    super.initState();
+    getController.allCategories();
+  }
+
   @override
   Widget build(BuildContext context) {
-    var heightScreen = MediaQuery.of(context).size.height;
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        toolbarHeight: 5,
+        backgroundColor: Colors.white,
+        title: const Text("Store"),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(60),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              onChanged: (value) {
+                getController.updateSearch(value);
+              },
+              decoration: InputDecoration(
+                hintText: "Search image...",
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                const Icon(
-                  Icons.location_on_outlined,
-                  size: 40,
-                ),
-                const Text(
-                  "Service Address",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                Spacer(),
-                IconButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ProfilePage(),
-                          ));
-                    },
-                    icon: const Icon(
-                      Icons.notifications_none_outlined,
-                      size: 40,
-                    ))
-              ],
+          Row(
+            children: [
+              GlobalWidget.WorkNameText(context, "All Categories"),
+              Spacer(),
+              GlobalWidget.SeeAllCategories(() {
+                Get.to(AllCategories());
+              }, context, "See All"),
+              SizedBox(
+                width: 20,
+              )
+            ],
+          ),
+          SizedBox(
+            height: 250,
+            child: Expanded(
+              child: Obx(() {
+                return StreamBuilder<QuerySnapshot>(
+                  stream: getController.filteredImages,
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                    final docs = snapshot.data!.docs;
+                    if (docs.isEmpty) {
+                      return Center(child: Text("No matching results"));
+                    }
+                    return GridView.builder(
+                      itemCount: docs.length,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 4,
+                        mainAxisSpacing: 1,
+                        crossAxisSpacing: 1,
+                        childAspectRatio: 0.8,
+                      ),
+                      padding: const EdgeInsets.all(5),
+                      itemBuilder: (context, index) {
+                        var data = docs[index];
+                        return InkWell(
+                          onTap: () {
+                            Get.to(ClickOnCategories());
+                          },
+                          child: Card(
+                            color: Colors.white,
+                            elevation: 1,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Expanded(
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(5),
+                                    child: Image.network(data['url']),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                    left: 2,
+                                    right: 2,
+                                  ),
+                                  child: Text(
+                                    data['name'],
+                                    style: TextStyle(fontSize: 10),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                );
+              }),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Card(
-              elevation: 5,
-              shape: RoundedRectangleBorder(
-                side: BorderSide(color: Colors.blue.shade100),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: TextField(
-                decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.search),
-                    hintText: "Search",
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(10),
-                    )),
-              ),
-            ),
+          Row(
+            children: [
+              GlobalWidget.WorkNameText(context, "Best Services"),
+              Spacer(),
+              GlobalWidget.SeeAllCategories(() {}, context, "See All"),
+              SizedBox(width: 20),
+            ],
           ),
-          Expanded(
-              child: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    GlobalWidget.WorkNameText(context, "All Category"),
-                    Spacer(),
-                    GlobalWidget.SeeAllCategories(() {}, context, "See All"),
-                    SizedBox(
-                      width: 20,
-                    )
-                  ],
-                ),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        GlobalWidget.AllCategory(
-                          context,
-                          "Carpenter",
-                          "assets/image/carpernter_bc.png",
-                          () {},
-                        ),
-                        GlobalWidget.AllCategory(
-                          context,
-                          "Cleaner",
-                          "assets/image/cliner.png",
-                          () {},
-                        ),
-                        GlobalWidget.AllCategory(
-                          context,
-                          "Painter",
-                          "assets/image/painter_bc.png",
-                          () {},
-                        ),
-                        GlobalWidget.AllCategory(
-                          context,
-                          "Electrician",
-                          "assets/image/electric_bc.png",
-                          () {},
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        GlobalWidget.AllCategory(
-                          context,
-                          "Ac Repair",
-                          "assets/image/ac_repairing_bc.png",
-                          () {},
-                        ),
-                        GlobalWidget.AllCategory(
-                          context,
-                          "Plumber",
-                          "assets/image/plumber.png",
-                          () {},
-                        ),
-                        GlobalWidget.AllCategory(
-                          context,
-                          "Men's Salon",
-                          "assets/image/salon.jpeg",
-                          () {},
-                        ),
-                        GlobalWidget.AllCategory(
-                          context,
-                          "Beauty",
-                          "assets/image/beauty_bc.png",
-                          () {},
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 15, right: 20),
-                  child: Row(
-                    children: [
-                      GlobalWidget.WorkNameText(context, "Best Service"),
-                      Spacer(),
-                      GlobalWidget.SeeAllCategories(() {}, context, "See All")
-                    ],
-                  ),
-                ),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      SizedBox(
-                        height: heightScreen * 0.4,
-                        child: Card(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              GlobalWidget.BestServicesImage1(
-                                context,
-                                "assets/image/kitchen.jpeg",
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 8),
-                                child: RatingBar.builder(
-                                  // minRating: 0,
-                                  // direction: Axis.horizontal,
-                                  allowHalfRating: true,
-                                  itemCount: 5,
-                                  itemSize: 30,
-                                  itemBuilder: (context, _) => Icon(
-                                    Icons.star,
-                                    color: Colors.blue,
-                                  ),
-                                  onRatingUpdate: (rating) {
-                                    print('User selected rating: $rating');
-                                  },
-                                ),
-                              ),
-                              GlobalWidget.WorkNameText(
-                                  context, "Complete Kitchen Clinaning"),
-                              Row(
-                                children: [
-                                  GlobalWidget.TextSpanTextOriginal(
-                                      context, "150", "50"),
-                                  SizedBox(
-                                    width: 80,
-                                  ),
-                                  GlobalWidget.ServicesProvideAddButton(
-                                      () {}, context, "Add")
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: heightScreen * 0.4,
-                        child: Card(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              GlobalWidget.BestServicesImage1(
-                                context,
-                                "assets/image/kitchen.jpeg",
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 8),
-                                child: RatingBar.builder(
-                                  // minRating: 0,
-                                  // direction: Axis.horizontal,
-                                  allowHalfRating: true,
-                                  itemCount: 5,
-                                  itemSize: 30,
-                                  itemBuilder: (context, _) => const Icon(
-                                    Icons.star,
-                                    color: Colors.blue,
-                                  ),
-                                  onRatingUpdate: (rating) {
-                                    print('User selected rating: $rating');
-                                  },
-                                ),
-                              ),
-                              GlobalWidget.WorkNameText(
-                                  context, "Complete Kitchen Clinaning"),
-                              Row(
-                                children: [
-                                  GlobalWidget.TextSpanTextOriginal(
-                                      context, "150", "50"),
-                                  SizedBox(
-                                    width: 80,
-                                  ),
-                                  GlobalWidget.ServicesProvideAddButton(
-                                      () {}, context, "Add")
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                GlobalWidget.BestServicesImage1(
-                  context,
-                  "assets/image/kitchen.jpeg",
-                ),
-              ],
-            ),
-          ))
         ],
       ),
     );
