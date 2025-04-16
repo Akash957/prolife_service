29eea25c1997f9d820f00553e9f1271628262702
Akash957 <157791836+Akash957@users.noreply.github.com>
2025-04-16 10:59:05 +0530

    work in profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:prolife_service/provider/location_provider.dart';
+import 'package:prolife_service/provider/profile_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:prolife_service/provider/auth_provider.dart';
 import 'package:prolife_service/view/screen/splash_screen.dart';
 import 'firebase_options.dart';
+import 'getx_service/language_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Get.put(LanguageService());
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -21,6 +27,8 @@ void main() async {
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => ProfileProvider()),
+        ChangeNotifierProvider(create: (_) => LocationProvider()),
       ],
       child: const MyApp(),
     ),
