13923a7c1b925c0536a1ad30cfec3df8d1d8a769
Akash957 <157791836+Akash957@users.noreply.github.com>
2025-05-24 14:37:36 +0530

    File name is changed

--- lib/notification/send_notification_ToPartners.dart
+++ lib/notification/send_notification_ToPartners.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
-import 'get_service_key.dart';
+import 'firebase_fcm_service.dart';
 
 class SendNotificationToPartners {
   var baseUrl =
